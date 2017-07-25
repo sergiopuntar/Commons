768e9101eb7e5a4f68616128cebf3e1d8b961829
Sergio Puntar <serginhopuntar@gmail.com>
2017-07-25 12:34:59 +0200

    Ajustes na configuração do Travis CI para execução do Sonar
    Remoção da execução por addon e inclusão dos parâmetros de organização e
    token no script after success

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ]; then
-    mvn sonar:sonar -P sonar
+    mvn sonar:sonar -P sonar -Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.login=$SONAR_TOKEN
 fi
