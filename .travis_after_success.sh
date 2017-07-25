58aca69b22fe69db820093008e7824b755079a85
Sergio Puntar <serginhopuntar@gmail.com>
2017-07-25 15:36:39 +0200

    Ajustes no script de execução do Sonar

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
-if [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ]; then
+if [ "$TRAVIS_JDK_VERSION" = "oraclejdk8" ];
+then
+   echo "Starting Sonar Analysis"
    mvn sonar:sonar -P sonar -Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.login=$SONAR_TOKEN
+else
+   echo "Skipping Sonar Analysis"
 fi
