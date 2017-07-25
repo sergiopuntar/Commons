a1f8dc29fcee7cb0e92247c334d20a5ea620c7eb
Sergio Puntar <serginhopuntar@gmail.com>
2017-07-25 13:02:45 +0200

    Ajustes na execução do Sonar
    Mudança no shebang do script de after success e ajuste fino na
    configuração do profile do Sonar

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 if [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ]; then
-    mvn sonar:sonar -P sonar -Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.login=$SONAR_TOKEN
+   mvn sonar:sonar -P sonar -Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.login=$SONAR_TOKEN
 fi
